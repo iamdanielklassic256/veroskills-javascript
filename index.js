8bd184d38045c7eb5ded104bc2fc889d85ec6621
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-06-28 15:49:16 +0300

    type conversion & ceorcion

--- index.js
+++ index.js
@@ -22,19 +22,52 @@
 // const math = Math.pow(2,3)
 // console.log(math)
 
-const currentYear = 2024
-const tenYearsLater = 2024 + 10
+// const currentYear = 2024
+// const tenYearsLater = 2024 + 10
 
 
-// Operator Precedence
+// // Operator Precedence
 
-// DRY - DONT REPEAT YOURSELF
-let x, y;
+// // DRY - DONT REPEAT YOURSELF
+// let x, y;
 
-x = y = 10 + 5;
-console.log(x); // 15
-console.log(y); // 15
+// x = y = 10 + 5;
+// // console.log(x); // 15
+// // console.log(y); // 15
 
-const averageNum = (10 + 10) / 2
-console.log(averageNum) // 10
+// const averageNum = (10 + 10) / 2
+// // console.log(averageNum) // 10
 
+// // SWITCH STATEMENT
+
+// let day = "Tuesday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("Monday is a workday");
+//     break;
+//   case "Tuesday":
+//   case "Wednesday":
+//   case "Thursday":
+//   case "Friday":
+//     console.log("It's a weekday");
+//     break;
+//   case "Saturday":
+//   case "Sunday":
+//     console.log("It's the weekend");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// ********* TYPE CONVERSION & TYPE coercion********************************  //
+
+// type conversion
+const inputYear = '2024';
+// console.log(inputYear + 19)
+console.log(typeof Number(inputYear))
+console.log(Number(inputYear) + 19)
+
+
+
+// type coercion
