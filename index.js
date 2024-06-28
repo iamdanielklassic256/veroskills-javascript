230292a1741abf4a656b6fea0ed268f361006379
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-06-28 10:32:47 +0300

    comparison operators

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 //Logging Hello World to the console
-console.log("Hello World");
+// console.log("Hello World");
+
+// console.log("Daniel");
+// console.log(5)
+// console.log(true)
+// console.log(false)
+
+
+// const firstName = "Daniel";
+// let lastName = "Comboni";
+
+// PRIMITIVE DATA TYPE
+// console.log(0>null) //false
+// console.log(0>=null) //true
+// console.log(0===null) //false
+// console.log(0==null) //false
+// console.log(0<null) //false
+// console.log(0<=null) //true
+
+
+// const math = Math.pow(2,3)
+// console.log(math)
+
+const currentYear = 2024
+const tenYearsLater = 2024 + 10
+
+
+// Operator Precedence
+
+// DRY - DONT REPEAT YOURSELF
+let x, y;
+
+x = y = 10 + 5;
+console.log(x); // 15
