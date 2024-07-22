d36f09b214c1a039c3682732d5e2a562929128cf
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-07-22 09:03:42 +0300

    getting started with beyond js basics

---
+++ arrayMethod.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const books = [
+  { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+  { title: "The Art of Gathering", author: "Priya Parker" },
+  { title: "1984", author: "George Orwell" },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+  { title: "Atomic habits", author: "James Clear" },
+];
+
+const names = ["abra", "bo", "guil", "hanna", "jacob", "kayla"];
