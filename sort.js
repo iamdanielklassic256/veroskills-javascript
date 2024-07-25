6a32e18ded846f2ff4b64c447e1150cc5c94665e
Okumu Daniel Comboni <okumucomboni@gmail.com>
2024-07-25 16:44:48 +0300

    sort

---
+++ sort.js
@@ -0,0 +1,18 @@
+//sort method
+const numbers = [5, 3, 8, 2, 1];
+const fruits = ["pear", "banana", "apple", "orange", "kiwi"]
+
+const people = [
+	{name: 'Daniel', age:70},
+	{name: 'John', age: 30},
+	{name: 'Jesse', age: 24},
+	{name: 'Jeff', age: 67},
+	{name: 'Yvonne', age: 75}
+]
+
+console.log(numbers.sort());
+
+fruits.sort().reverse()
+console.log(fruits);
+
+people.sort()
